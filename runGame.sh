bc96d45895b244ae86564dbe4b6f01fc0ff24aba
Nick Malaguti <nmalaguti@users.noreply.github.com>
2016-11-25 08:22:34 -0500

    Add more bots to runGame.sh

--- runGame.sh
+++ runGame.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-rm -f *.log *.hlt
+rm -f *.log* *.hlt
 
-(
-    cd java
-    javac MyBot.java
-)
 (
     cd kotlin
     ./gradlew installDist
 )
 
-./halite -d "50 50" \
-    "java -cp ./stable/javabot MyBot" \
-    "python stable/jsbot/MyBot.py" \
-    "python stable/kotlinbot/MyBot.py" \
-    "java -jar kotlin/build/install/MyBot/MyBot.jar"
+# -s 515525598
+
+./halite -d "40 40" \
+    "tournament/bots/available/frugalbot/run.sh" \
+    "java -XX:+PrintGCDetails -Xloggc:gc.log -Xmx250m -jar kotlin/build/install/MyBot/MyBot.jar" \
+
+
+
+#    "tournament/bots/available/smarterbot/run.sh" \
+#    "tournament/bots/available/battlebot/run.sh" \
+#    "tournament/bots/available/betterbot/run.sh" \
+
+
+
+
+#    "tournament/bots/available/betterbot/run.sh" \
+#    "java -jar stable/betterbot/MyBot.jar" \
+#    "java -cp ./stable/javabot MyBot" \
+#    "python stable/jsbot/MyBot.py" \
+#    "python stable/kotlinbot/MyBot.py" \
