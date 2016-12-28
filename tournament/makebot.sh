282725d7fb57effdfb2077e3c62eaa8709984103
Nick Malaguti <nmalaguti@users.noreply.github.com>
2016-12-27 22:07:23 -0500

    Deal with existing directories

--- tournament/makebot.sh
+++ tournament/makebot.sh
@@ -2,8 +2,10 @@
 
 cd "$(dirname "$0")"
 
-mkdir "bots/available/$1"
+
+mkdir -p "bots/available/$1"
 cp bots/available/frugalbot/run.sh "bots/available/$1/"
+(cd ../kotlin; gw installDist;)
 cp ../kotlin/build/install/MyBot/MyBot.jar "bots/available/$1/"
-ssh nmalaguti.com "mkdir ~/git/halite-bots/tournament/bots/available/$1"
+ssh nmalaguti.com "mkdir -p ~/git/halite-bots/tournament/bots/available/$1"
 scp bots/available/$1/* nmalaguti.com:git/halite-bots/tournament/bots/available/$1/
