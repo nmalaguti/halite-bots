3c923f609fe6e0fb2f592f0e693a22c5fe1696d7
Nick Malaguti <nmalaguti@users.noreply.github.com>
2016-12-27 22:07:40 -0500

    Added more scenarios

--- runGame.sh
+++ runGame.sh
@@ -7,38 +7,35 @@ rm -f *.log* *.hlt
     ./gradlew installDist
 )
 
-# -s 515525598
-# -s 3528423518 # interesting fighting
-# -s 3185805234
-# -s 726503236
-# -s 3155813572
+##########################
 
-# -s 4192888241
-# -s 3313079889
-
-
-# -d "35 35" -s 1664545737 # shoving test
-# -d "35 35" -s 747897332  # test for crowd control
-# -d "35 35" -s 3761909030 # glow, swarm, blacklist, grad = expand when stalling
-# -d "45 45" -s 1541902692 # 3 players for walking bots
-# -d "35 35" -s 41055851 # 2x avg bot and walking bot - careful about overextending into env
-# -d "35 35" -s 2333994507 # absolute massacre by directedwalk
-
-# 783788386
-
-# 413706665
-
-# 1517492438
-
-# 1511607887
-
-# 2023371628 # bad for finer, really good for average based bots
-
-./halite -d "35 35" -s 1286846703 \
-    "tournament/bots/available/fighterbot/run.sh" \
-    "tournament/bots/available/thugbot/run.sh" \
-    "tournament/bots/available/superchargerbot/run.sh" \
+./halite -d "35 35" -s 4074769314 \
     "java -XX:+PrintGCDetails -Xloggc:gc.log -Xmx250m -jar kotlin/build/install/MyBot/MyBot.jar" \
+    "tournament/bots/available/orientationbot/run.sh" \
+    "tournament/bots/available/seekerbot/run.sh" \
+
+# lots of prod = 0. good for testing 255 constrained combat
+#./halite -d "35 35" -s 574898733 \
+#    "tournament/bots/available/thugbot/run.sh" \
+#    "java -XX:+PrintGCDetails -Xloggc:gc.log -Xmx250m -jar kotlin/build/install/MyBot/MyBot.jar" \
+
+#./halite -d "35 35" -s 3614115138 \
+#    "tournament/bots/available/seekerbot/run.sh" \
+#    "java -XX:+PrintGCDetails -Xloggc:gc.log -Xmx250m -jar kotlin/build/install/MyBot/MyBot.jar" \
+
+#./halite -d "35 35" -s 2811877567 \
+#    "java -XX:+PrintGCDetails -Xloggc:gc.log -Xmx250m -jar kotlin/build/install/MyBot/MyBot.jar" \
+#    "tournament/bots/available/seekerbot/run.sh" \
+
+#./halite -d "35 35" -s 4030606183 \
+#    "tournament/bots/available/seekerbot/run.sh" \
+#    "java -XX:+PrintGCDetails -Xloggc:gc.log -Xmx250m -jar kotlin/build/install/MyBot/MyBot.jar" \
+
+#./halite -d "35 35" -s 1006313739 \
+#    "tournament/bots/available/orientationbot/run.sh" \
+#    "tournament/bots/available/thugbot/run.sh" \
+#    "tournament/bots/available/thugbot/run.sh" \
+#    "java -XX:+PrintGCDetails -Xloggc:gc.log -Xmx250m -jar kotlin/build/install/MyBot/MyBot.jar" \
 
 # used for optimizing sort order for targets
 #./halite -d "35 35" -s 1286846703 \
